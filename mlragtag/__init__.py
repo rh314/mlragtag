76da522a1b54ce60b092a1e334b53a955e3e5fac
Ruan Havenstein <rh314@users.noreply.github.com>
2018-07-30 08:39:44 +0200

    - Generalize random_pad_2n by extracting random_pad function - Implement a pytorch NN class to create simple quick-and-dirty multi-layer   relu networks together with their optimizers.

--- mlragtag/__init__.py
+++ mlragtag/__init__.py
@@ -3,6 +3,7 @@
 
 import random
 import numpy as np
+import torch
 
 
 class IdxSplitter():
@@ -55,13 +56,11 @@ def ceil_power2(n):
     return 2 ** int(np.ceil(np.log2(n)))
 
 
-def random_pad_2n(items, pad_value=None, pad_is_func=False):
+def random_pad(items, new_len, pad_value=None, pad_is_func=False):
     '''
-    Randomly intersperses None into the list so that it's length is an integer
-    power of 2.
+    Randomly intersperses None into the list so that it's length is new_len.
     '''
     old_len = len(items)
-    new_len = ceil_power2(old_len)
     mask = [1] * old_len + [0] * (new_len - old_len)
     np.random.shuffle(mask)
     mask_counter = -1
@@ -76,6 +75,16 @@ def random_pad_2n(items, pad_value=None, pad_is_func=False):
     return new_items
 
 
+def random_pad_2n(items, pad_value=None, pad_is_func=False):
+    '''
+    Randomly intersperses None into the list so that it's length is an integer
+    power of 2.
+    '''
+    old_len = len(items)
+    new_len = ceil_power2(old_len)
+    return random_pad(items, new_len, pad_value, pad_is_func)
+
+
 def binary_fold(fold_fn, items):
     '''
     Repeatedly combine every two items into one using fold_fn until there is
@@ -85,9 +94,52 @@ def binary_fold(fold_fn, items):
         raise Exception('Length of items must be a power of 2')
     items = items[:]
     while len(items) > 1:
-        print(items)
         for k in range(len(items) // 2):
             items[k] = fold_fn(items[2 * k], items[2 * k + 1])
         del items[len(items) // 2:]
-    print(items)
     return items[0]
+
+
+lrelu = torch.nn.LeakyReLU(0.3)
+sigmoid = torch.nn.Sigmoid()
+
+
+class SimpleNetwork(torch.nn.Module):
+    '''
+    Very quick-and dirty multi layer network.
+    '''
+    def __init__(self, sizes, has_optimizer=True):
+        super().__init__()
+        layers = []
+        for k, (d_in, d_out) in enumerate(zip(sizes[:-1], sizes[1:])):
+            layer = torch.nn.Linear(d_in, d_out)
+            layers.append(layer)
+            setattr(self, f'linear{k}', layer)
+        self.layers = layers
+        if has_optimizer:
+            self.lr = 1e-3
+            self.rebuild_optimizer()
+
+    def forward(self, inpt, final_sigmoid=False):
+        res = inpt
+        for k, layer in enumerate(self.layers):
+            res = layer(res)
+            # don't apply (l)relu activation on final layer
+            if k != len(self.layers):
+                res = lrelu(res)
+        self.last_result = res  # Save last result (never fsigmoid)
+        if final_sigmoid:
+            res = sigmoid(res)
+        return res
+
+    def rebuild_optimizer(self):
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
+
+    def step(self):
+        self.optimizer.step()
+
+    def backward_on(self, loss, do_zero_grad=True):
+        if do_zero_grad:
+            self.zero_grad()
+        loss.backward()
+        self.optimizer.step()
