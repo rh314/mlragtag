3d5bba18135c586741ff1a0176e52ae2f65911f7
Ruan Havenstein <rh314@users.noreply.github.com>
2018-08-26 22:09:41 +0200

    WIP - List logging type.
    Did some experiments. 50 rows is a good amount to simultaneously insert.
    About 3x faster than 1 row at a time.

--- mlragtag/loggings.py
+++ mlragtag/loggings.py
@@ -1,4 +1,74 @@
 import psycopg2
+import time
+import struct
+import numpy as np
+
+
+db_holder = [None]
+
+
+def get_db():
+    '''
+    Initialize db if not connected yet.
+    PS: Need to setup postgres first:
+        cd ~/postgres/bin
+        ./createuser -e mlragtag
+        ./createdb -e -Omlragtag mlragtag_db
+    '''
+    if db_holder[0] is None:
+        db = psycopg2.connect(
+            "dbname='mlragtag_db' user='mlragtag' host='localhost'")
+        db_holder[0] = db
+        with db.cursor() as cur:
+            cur.execute("SET synchronous_commit = 'on'")
+    return db_holder[0]
+
+
+def new_uuid():
+    return struct.pack('d', time.time()).hex()
+
+
+class List():
+    # PS: Per row fastest insert is if you extend with about 50 items at a
+    # time.
+    def __init__(self):
+        self.uuid = 't' + new_uuid()
+        db = get_db()
+        with db.cursor() as cur:
+            cur.execute('''
+                CREATE TABLE %s(tstamp REAL, value REAL)
+                ''' % self.uuid)
+            db.commit()
+
+    def append(self, x):
+        if not isinstance(x, (float, int)):
+            raise TypeError
+        tstamp = time.time()
+        db = get_db()
+        with db.cursor() as cur:
+            cur.execute(
+                'INSERT INTO %s VALUES(%f, %f)' %
+                (self.uuid, tstamp, x))
+
+    def extend(self, x):
+        rowdata = []
+        tstamp = time.time()
+        for xi in x:
+            rowdata.extend((tstamp, xi))
+            if not isinstance(xi, (float, int)):
+                raise TypeError
+        db = get_db()
+        cmd = 'INSERT INTO %s VALUES(%%s, %%s)' % self.uuid
+        cmd = ';'.join(((cmd,) * len(x)))
+        with db.cursor() as cur:
+            cur.execute(cmd, rowdata)
+
+    def get_values(self):
+        db = get_db()
+        with db.cursor() as cur:
+            cur.execute('SELECT value FROM %s' % self.uuid)
+            res = [_[0] for _ in cur.fetchall()]
+        return res
 
 
 class MovingAverage():
